fbbb461c46129645817cced45f4405a98403c625
alistairyves <alistairyves@gmail.com>
2014-10-23 09:25:45 -0400

    initial commit

---
+++ soen472proj/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soen472proj
+{
+    public class Class1
+    {
+    }
+}
